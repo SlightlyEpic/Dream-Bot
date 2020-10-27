62264300f7df73903ed01d8e492dd001708cb348
SlightlyEpic <42976178+SlightlyEpic@users.noreply.github.com>
2020-10-27 12:36:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ client.on("ready", () => {
 
 	client.user.setPresence({
 		activity: {
-			name: "you! rHelp",
+			name: `you! ${config.default_prefix}Help`,
 			type: "WATCHING"
 		},
 		status: "online"
